043ba9baa3c2761f31ea4b4873f78157a29b74cc
Young <wu551900@gmail.com>
2022-09-20 01:01:41 +0800

    初始化 Spring Boot project

---
+++ src/main/java/com/youngwu/todolistv2/ToDoListV2Application.java
@@ -0,0 +1,13 @@
+package com.youngwu.todolistv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/youngwu/todolistv2/ToDoListV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngwu.todolistv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
